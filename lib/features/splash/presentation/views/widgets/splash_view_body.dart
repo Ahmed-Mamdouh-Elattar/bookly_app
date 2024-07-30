18c5043f5dff592bd77c17b84208032898b3989a
Ahmed Mamdouh <ahmed.mamdouh.elattar@gmail.com>
2024-07-30 15:55:15 +0300

    create and navigate to home view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,19 +1,42 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/custom_text_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBodey extends StatelessWidget {
+class SplashViewBodey extends StatefulWidget {
   const SplashViewBodey({super.key});
+
+  @override
+  State<SplashViewBodey> createState() => _SplashViewBodeyState();
+}
+
+class _SplashViewBodeyState extends State<SplashViewBodey> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHomeView();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(Assets.logo),
-        const CustomTextAnimation(),
-      ],
-    );
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(Assets.logo),
+          const CustomTextAnimation(),
+        ]);
+  }
+
+  Future<Null> navigateToHomeView() {
+    return Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        const HomeView(),
+        transition: Transition.fadeIn,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
